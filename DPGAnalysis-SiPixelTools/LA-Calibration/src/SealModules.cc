9b86fbacbc28621d3601599b7b0430022301f146
Tamas Vami <tamas.almos.vami@cern.ch>
2020-05-30 17:52:24 -0600

    LA-Calibration

---
+++ DPGAnalysis-SiPixelTools/LA-Calibration/src/SealModules.cc
@@ -0,0 +1,8 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "../interface/SiPixelLorentzAngle.h"
+#include "../interface/SiPixelLorentzAngleDB.h"
+
+
+DEFINE_FWK_MODULE(SiPixelLorentzAngle);
+DEFINE_FWK_MODULE(SiPixelLorentzAngleDB);
---
+++ DPGAnalysis-SiPixelTools/LA-Calibration/test/SiPixelLorentzAngleReader_cfg.py
@@ -0,0 +1,68 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Test")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("EmptyIOVSource",
+    lastRun = cms.untracked.uint32(1),
+    timetype = cms.string('runnumber'),
+    firstRun = cms.untracked.uint32(1),
+    interval = cms.uint32(1)
+)
+
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("histo.root")
+                                   )
+
+
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.Timing = cms.Service("Timing")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.GeometryIdeal_cff")
+process.GlobalTag.globaltag = "GR_R_73_V0A::All"
+
+
+process.QualityReader = cms.ESSource("PoolDBESSource",
+#    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0),
+        authenticationPath = cms.untracked.string('')
+    ),
+    toGet = cms.VPSet(
+		cms.PSet(
+			record = cms.string("SiPixelLorentzAngleRcd"),
+			tag = cms.string("SiPixelLorentzAngle_v4_offline")
+		),
+		cms.PSet(
+			record = cms.string("SiPixelLorentzAngleSimRcd"),
+			tag = cms.string("SiPixelLorentzAngleSim_v02_mc")
+		)
+	),
+    connect = cms.string('sqlite_file:SiPixelLorentzAngle.db')
+)
+
+#process.es_prefer_QualityReader = cms.ESPrefer("PoolDBESSource","QualityReader")
+
+process.LorentzAngleReader = cms.EDAnalyzer("SiPixelLorentzAngleReader",
+    printDebug = cms.untracked.bool(False),
+    useSimRcd = cms.bool(False)
+)
+
+process.LorentzAngleSimReader = cms.EDAnalyzer("SiPixelLorentzAngleReader",
+    printDebug = cms.untracked.bool(False),
+    useSimRcd = cms.bool(True)
+                                             
+)
+
+
+process.p = cms.Path(process.LorentzAngleReader*process.LorentzAngleSimReader)
+
