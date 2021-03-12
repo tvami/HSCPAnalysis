e926de16bd9bf5419c3f23a3567fbfea56e8e860
Tamas Vami <tamas.almos.vami@cern.ch>
2021-03-11 21:18:19 -0500

    Update lunch.sh

--- DataAna/lunch.sh
+++ DataAna/lunch.sh
@@ -1,4 +1,4 @@
 cd .. 
 scram b -j 
 cd -
- cmsRun 5runHSCPAnalyzer.py runNumber=315690
+cmsRun 5runHSCPAnalyzer.py inputFileName=root://cms-xrd-global.cern.ch//store/user/tvami/PixelTrees/SingleMuon/crab_ALCARECO_2018D_PixelTrees_2021Febr_v1-NoMT/210204_194102/0003/PixelTree_3544.root runNumber=324773 
